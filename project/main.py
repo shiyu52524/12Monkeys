fc9d167f812e1123bd50aa8d4bad7e130f7b0865
shiyu52524 <381852524@qq.com>
2019-08-09 17:07:48 +0800

    2019/8/9

--- project/main.py
+++ project/main.py
@@ -1,15 +1,21 @@
 from project.multiprogress_handler import mutiprogress
 from project.read2redis import read_data
-
-
-
-
-
+from project.response_handler import tag_seletor
 
 if __name__ == '__main__':
     read = read_data()
     read.path = r"..\docs\test.csv"
     read.read_csv()
 
+
+
     mult= mutiprogress()
+    mult.core = 8
     mult.MonkeyKing()
+    tool = tag_seletor()
+
+
+
+    tool.rule = '#pro_info_1  tr:nth-child(1) td p'
+    tool.result_tag = 'result1'
+    tool.nomal_search()
--- project/response_handler.py
+++ project/response_handler.py
@@ -1,1 +1,43 @@
-from bs4 import BeautifulSoup
+from project.config.mongoDB import *
+from bson import ObjectId
+
+class tag_seletor():
+    def __init__(self):
+        self.author = 'shiyu52524'
+        self.rule = ''
+        self.result_tag = ''
+
+
+
+    def selector(self,content,bs4_selector=True,xpath_selector=False):
+        if bs4_selector:
+            from bs4 import BeautifulSoup
+            soup = BeautifulSoup(content,'lxml')
+            aim = soup.select(self.rule)
+            print(aim)
+            for i in aim:
+                if i:
+                    print(i.text)
+                    result = i.text
+                    return result
+        # if xpath_selector:
+        #
+        #     selector = etree.HTML(content)
+        #     aim = selector.xpath(self.rule)
+        #     print(aim)
+        #     return aim
+
+    def update2mongo(self,id,data):
+        dict = {}
+        dict[self.result_tag] = data
+        result = mycol.update_many({"_id": ObjectId(id)}, {"$set": dict})
+        print(result)
+
+
+
+    def nomal_search(self):
+        for item in mycol.find():
+            content = item['response']
+            data = self.selector(content)
+            id = item['_id']
+            self.update2mongo(id,data)
